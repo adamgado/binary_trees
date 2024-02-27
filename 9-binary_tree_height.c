dbb11b3f0bc3f7af55f30bf3e840124374dd4314
adamgado <adamgado33@gmail.com>
2024-02-27 01:34:35 -0800

    9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measure height of binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * Return: height of binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_len = 0;
+	size_t right_len = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_len = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_len = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((left_len > right_len) ? left_len : right_len);
+}
