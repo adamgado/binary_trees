40a61d159f69ad5ad0ac40c4d76e5c64c72fa67c
adamgado <adamgado33@gmail.com>
2024-02-27 01:12:35 -0800

    6

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,5 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
-
 #include <stddef.h>
 #include <stdlib.h>
 /**
@@ -30,5 +29,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
