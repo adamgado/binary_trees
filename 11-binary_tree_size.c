d1b8b6a15beb00ec4848881510654bfa5bbef20e
adamgado <adamgado33@gmail.com>
2024-02-28 07:25:03 -0800

    11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - measure size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size of
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree != NULL)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
